50536a6f180c172238ebf4f8154877e3fcab7a0d
Bipin Mishra <bipinmm165@gmail.com>
2026-06-29 17:02:16 +0530

    chore: audio component fix

--- components/MyAudio.tsx
+++ components/MyAudio.tsx
@@ -1,6 +1,6 @@
 const Playlist = () => {
   return (
-    <section className="py-5">
+    <section className="pb-7">
       <div className="mx-auto max-w-xl rounded-3xl border border-white/10 bg-zinc-900 p-6">
         <h2 className="mb-4 text-2xl font-bold text-white">
           🎵 My Song
